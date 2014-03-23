d7b78114323b7d21b83d3368753151d0cf13a45b
dustinbrown <dustinjamesbrown@gmail.com>
2014-03-23 11:26:21 -0700

    first commit

---
+++ retrieve4me.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+
+class Retrieve4me
+end
---
+++ spec/retrieve4me_spec.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+
+require_relative '../retrieve4me.rb'
+
+describe 'Retreive4me' do
+  let(:app) { Retrieve4me.new }
+end
