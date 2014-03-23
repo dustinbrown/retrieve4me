8bcd4a7464baf771fd39ba54973af23cefce54e3
dustinbrown <dustinjamesbrown@gmail.com>
2014-03-23 15:58:50 -0700

    renamed spec test and added comments in lib/filesystem_parser.rb

--- lib/filesystem_parser.rb
+++ lib/filesystem_parser.rb
@@ -3,10 +3,8 @@ require_relative '../myconfig.rb'
 
 class Filesystem_Parser
 
-
   @@items_on_disk = []
-  
-  
+
   def initialize#(config)
     #load config files
     config = MyConfig.new
@@ -15,38 +13,44 @@ class Filesystem_Parser
     #code
     # set items_on_disk
   end
-  
+
   def get_items
     @@items_on_disk
   end
-  
+
   def set_items
-    #Build array
+    #Build array and store it in a class array
     get_items_in_path(@path).each do |item|
-      #next if item =~ /^\./
       @@items_on_disk << item
     end
     @@items_on_disk
   end
-  
+
   def get_items_in_path(path)
-    #Dir.open(path).to_a if File.directory? path
+    #does the path exist?
     if File.directory? path
+
+      #loop through files and directories in path
       Dir.open(path).to_a.each do |file_or_dir|
+        #skip dot files
         next if file_or_dir =~ /^\./
+
+        #convienence var
         full_path = "#{path}/#{file_or_dir}"
-        
+
+        #Store file and directory names into instance var
         if File.directory?(full_path)
           @item_array << file_or_dir
+
+          #if full_path is a dir, use recursion to keep travelling into the directory
           get_items_in_path(full_path)
+
         else
           @item_array << file_or_dir
         end
-        
       end
     end
     @item_array
   end
-  
 end
 
