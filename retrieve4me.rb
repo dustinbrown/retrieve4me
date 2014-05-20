012545e4486b28bc7a11dc54feceb81a2594008f
dustinbrown <dustinjamesbrown@gmail.com>
2014-05-19 22:16:02 -0700

    added list and override options

--- retrieve4me.rb
+++ retrieve4me.rb
@@ -6,7 +6,7 @@ require 'json'
 #require 'pp'
 
 @log = Logger.new(STDOUT)
-@log.level = Logger::DEBUG
+@log.level = Logger::INFO
 @log.datetime_format = "%Y-%m-%d %H:%M:%S"
 
 opts = Trollop::options do
@@ -17,8 +17,10 @@ Usage:
 where [options] are:
 EOS
 
-  opt :dry, "Issue a dry-run"
   opt :all, "See all files listed by the rest end point"
+  opt :dry, "Issue a dry-run"
+	opt :list, "list rest files"
+	opt :override, "Enter override file name, can be regex", :type => :string
   #opt :file, "Extra data filename to read in, with a very long option description like this one",
   #      :type => String
   #opt :volume, "Volume level", :default => 3.0
@@ -31,19 +33,28 @@ end
 #Use END so we can use methods before they are defined..
 END {
 
-if opts[:dry] && opts[:all]
-  puts get_rest_data_pretty
-elsif opts[:dry]
+	puts get_rest_data_pretty 																	if opts[:dry] && opts[:all]
+
+	puts file_to_retrieve(get_rest_data_pretty, get_local_data) if opts[:dry]
+
+	puts get_rest_data_pretty 																	if opts[:list]
+
+	puts find_override(opts[:override], get_rest_data_pretty)		if opts[:override]
+
   #puts get_rest_data
   #puts get_rest_data_pretty
-  #puts get_local_data
+ # puts get_local_data
+	#lines_array = IO.readlines("/home/dbrown/git/retrieve4me/media.txt")
  # puts find_matches(get_rest_data_pretty, get_local_data)
-  puts file_to_retrieve(get_rest_data_pretty, get_local_data)
+  #puts file_to_retrieve(get_rest_data_pretty, get_local_data)
   #fs_parser.set_items
   #puts fs_parser.get_items
   #code
-end
 }
+
+def find_override(override, rest_array)
+	rest_array.select{|filename| filename.match(/#{override}/)}
+end
 
 def file_to_retrieve(rest_array, local_array)
   #requires 2 arrays
@@ -105,9 +116,14 @@ def get_rest_data_pretty
 end
 
 def get_local_data
-  fs_parser = Filesystem_Parser.new
-  fs_parser.set_items
-  fs_parser.get_items
+	if File.exists? "media.txt"
+		lines_array = IO.readlines("media.txt")
+		lines_array
+	else	
+  	fs_parser = Filesystem_Parser.new
+  	fs_parser.set_items
+  	fs_parser.get_items
+	end
 end
 #log.debug("get_items: #{fs_parser.get_items}")
 #log.debug("set_items: #{fs_parser.set_items}")
